1f6d37d7c1e34c82a38419ede4588e67efdcee9b
RafalSocha01 <141636422+RafalSocha01@users.noreply.github.com>
2025-11-20 11:41:45 +0100

    Załącznik nr.2 - Zadania testowe www.gwsh.pl

---
+++ Zalacznik nr.2 - Zadania testowe www.gwsh.pl.md/Zalacznik nr.2 - Zadania testowe www.gwsh.pl
@@ -0,0 +1,28 @@
+---
+id: Zalacznik nr.2 - Zadania testowe www.gwsh.pl
+title: Załącznik nr.2 - Zadania testowe www.gwsh.pl
+---
+
+# Zadania testowe [https://www.gwsh.pl/](https://www.gwsh.pl/) 
+Na odpowiedzi czekam do poniedziałku 3 lutego do godziny 12
+## Podstrony
+1.	[https://www.gwsh.pl/](https://www.gwsh.pl/)
+2.	[https://www.gwsh.pl/szukaj/] (https://www.gwsh.pl/szukaj/)
+3.	[https://www.gwsh.pl/kontakt/](https://www.gwsh.pl/kontakt/) 
+4.	[https://www.gwsh.pl/wydarzenia/](https://www.gwsh.pl/wydarzenia/) 
+5.	[https://www.gwsh.pl/wydarzenia/program-mobilnosci-akademickiej-kirgistan.html](https://www.gwsh.pl/wydarzenia/program-mobilnosci-akademickiej-kirgistan.html)
+6.	[https://www.gwsh.pl/kmn/kmn-nagranie_z_konferencji.html](https://www.gwsh.pl/kmn/kmn-nagranie_z_konferencji.html) 
+7.	[https://www.gwsh.pl/rekrutacja/](https://www.gwsh.pl/rekrutacja/)
+8.	[https://www.gwsh.pl/kursy-i-szkolenia/kurs/kurs_irydologii.html](https://www.gwsh.pl/kursy-i-szkolenia/kurs/kurs_irydologii.html)
+9.	[https://www.gwsh.pl/studia/psychologia.html](https://www.gwsh.pl/studia/psychologia.html)
+10.	[https://www.gwsh.pl/rekrutacja/terminy-pierwszych-zjazdow.html](https://www.gwsh.pl/rekrutacja/terminy-pierwszych-zjazdow.html)
+## Zadanie 1
+Na podstronie [https://www.gwsh.pl/kursy-i-szkolenia/kurs/kurs_irydologii.html](https://www.gwsh.pl/kursy-i-szkolenia/kurs/kurs_irydologii.html) podaj czas trwania kursu, termin i cenę za osobę. Czy napotkałeś\aś jakieś problemy? Jeśli tak opisz jakie.
+### Odpowiedź: 
+## Zadanie 2
+Na podstronie [https://www.gwsh.pl/studia/psychologia.html](https://www.gwsh.pl/studia/psychologia.html) odszukaj plik PDF z harmonogramem realizacji planu studiów dla studiów stacjonarnych i odczytaj z niego liczbę godzin przypadających na przedmiot Biologiczne podstawy zachowania. Czy napotkałeś\aś jakieś problemy? Jeśli tak opisz jakie.
+### Odpowiedź:
+## Zadanie 3
+Z pliku excel na podstronie [https://www.gwsh.pl/rekrutacja/terminy-pierwszych-zjazdow.html](https://www.gwsh.pl/rekrutacja/terminy-pierwszych-zjazdow.html) podaj terminy zjazdów dla kierunku Logistyka I stopnia w 5 i 7 semestrze. Czy napotkałeś\aś jakieś problemy? Jeśli tak opisz jakie.
+### Odpowiedź: 
+## Uwagi dodatkowe: 
